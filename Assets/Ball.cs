decabc1ec35bad316011ae280c3b84815e146eaa
jayson sweet <jayson_sweet15@yahoo.com>
2014-12-07 14:05:32 -0900

    Adding files to new test repo.

---
+++ Assets/Ball.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public bool touchingGround;
+	public float sphereRadius = 0.5f;
+
+	public Vector3 bounceVelocity = new Vector3( 0, 5, 0 );
+
+	// Use this for initialization
+	void Start () {
+		Funtion1 ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		touchingGround = IsTouching (); 
+			Debug.Log (IsTouching ());
+
+		if ( touchingGround == true ) {
+			Bounce( bounceVelocity );
+		}
+	}
+
+	void Funtion1 () {
+		float tempVar = 1.1f;
+		tempVar = 5.4f;   
+	}
+
+	void Bounce ( Vector3 bounceVelocity ) {
+		rigidbody.velocity = bounceVelocity;
+	}
+	bool IsTouching () {
+		return Physics.Raycast ( transform.position, Vector3.down, sphereRadius );
+	}
+
+}
+
---
+++ Assets/Syntax.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax : MonoBehaviour {
+
+	public bool booleanl1 = false;
+	private bool booleanl2 = true;
+
+	public int integer = 1;
+	public float floatingPoint = 1.1f;
+
+
+	// Use this for initialization
+	void Start () {
+	   if (booleanl1 == true) {
+			Debug.Log ("Awesome");
+		}
+
+		integer = 10;
+		floatingPoint = 5.4f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		integer = integer + 1;
+		integer += 1;
+	}
+}
